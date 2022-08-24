b7f221148e803709c0d2f98ee332667d5f8dba7c
Scott <sbferguson17@gmail.com>
2022-08-24 11:32:49 -0600

    add carouselStyles for Splideelements, add veggie component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import "./carouselStyles.css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import '@splidejs/react-splide/css';
 
@@ -30,73 +30,30 @@ function Popular() {
 
   return (
     <div>
-        <Wrapper>
+        <div className='wrapper'>
             <h3>Popular Picks</h3>
             <Splide options={{
-                perPage: 4,
+                perPage: 3,
                 arrows: false,
-                paginations: false,
+                pagination: false,
                 drag: "free",
                 gap: "5rem",
             }}>
             {popular.map((recipe) => {
                 return (
                     <SplideSlide key={recipe.id}>
-                        <Card>
+                        <div className='card'>
                             <p>{recipe.title}</p>
                             <img src={recipe.image} alt={recipe.title} />
-                        </Card>
-                        <Gradient />
+                        </div>
+                        <div className='gradient'/>
                     </SplideSlide>
                 )
             })}
             </Splide>
-        </Wrapper>
+        </div>
     </div>
   )
-}
-
-const Card = styled.div`
-    min-height: 25rem;
-    border-radius: 2rem;
-    overflow: hidden;
-    position: relative
-    img {
-        position: absolute;
-        border-radius: 2rem;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    p {
-        position: absolute;
-        z-index: 10;
-        left: 50%;
-        bottom: 0%;
-        transform: translate(-50%, 0%);
-        color: white;
-        width: 100%;
-        text-align: center;
-        font-weight: 600;
-        font-size: 1rem;
-        height: 40%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-`;
-
-const Gradient = styled.div`
-    z-index: 3;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
-`
-
-const Wrapper = styled.div`
-    margin: 4rem 0rem;
-`;
+};
 
 export default Popular
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
+import "./carouselStyles.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
+
 function Veggie() {
-  return (
-    <div>Veggie</div>
-  )
+
+  const [veggie, setVeggie] = useState([]);
+
+    useEffect(() => {
+        getVeggie();
+    }, [])
+
+    const getVeggie = async () => {
+        const check = localStorage.getItem('veggie');
+
+        if (check) {
+            setVeggie(JSON.parse(check))
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&tags=vegetarian&number=9`);
+            const data = await api.json();
+            localStorage.setItem('veggie', JSON.stringify(data.recipes));
+            setVeggie(data.recipes)
+            console.log(data.recipes);
+        }
+    }
+
+    return (
+      <div>
+          <div className='wrapper'>
+              <h3>Our Vegitarian Picks</h3>
+              <Splide options={{
+                  perPage: 3,
+                  arrows: false,
+                  pagination: false,
+                  drag: "free",
+                  gap: "5rem",
+              }}>
+              {veggie.map((recipe) => {
+                  return (
+                      <SplideSlide key={recipe.id}>
+                          <div className='card'>
+                              <p>{recipe.title}</p>
+                              <img src={recipe.image} alt={recipe.title} />
+                          </div>
+                          <div className='gradient'/>
+                      </SplideSlide>
+                  )
+              })}
+              </Splide>
+          </div>
+      </div>
+    )
 }
 
 export default Veggie
