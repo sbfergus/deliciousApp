8cd37866a94c34622f0b5a48dd4c7859bd516581
Scott <sbferguson17@gmail.com>
2022-08-24 07:32:04 -0600

    add local storage for testing

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -13,10 +13,19 @@ function Popular() {
     }, [])
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-        const data = await api.json();
-        console.log(data);
-        setPopular(data.recipes)
+        const check = localStorage.getItem('popular');
+
+        if (check) {
+            setPopular(JSON.parse(check))
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+            const data = await api.json();
+            localStorage.setItem('populat', JSON.stringify(data.recipes));
+            setPopular(data.recipes)
+            console.log(data.recipes);
+
+        }
+
     }
 
   return (
