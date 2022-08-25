e86c0df6b0d7b28fd83f6397b397dc4283dd91fa
Scott <sbferguson17@gmail.com>
2022-08-24 20:21:57 -0600

    nav styles centered

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -28,18 +28,43 @@ function Category() {
   )
 }
 
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+`;
+
 const SLink = styled(NavLink)`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4 {
+        color: white;
+        font-size: 0.8rem;
+        margin: 0;
+        margin-bottom: .25rem;
+    }
+
+    svg {
+        color: white;
+        font-size: 1.5rem;
+        margin: 0;
+        margin-bottom: .25rem;
+    }
+
+    
 `
 
-const List = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 2rem 0rem;
-`
+
 
 export default Category
